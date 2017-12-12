dbbb1554ea5f3f7bda695c959be7f20caded6814
Kavi Ravuri <kaviravuri888@gmail.com>
2017-12-11 22:54:44 -0600

    website

---
+++ main.js
@@ -0,0 +1,50 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBf17vSw0ov3krg1a13x4ZPK2nnXAx43ww",
+    authDomain: "leaderboard-2225f.firebaseapp.com",
+    databaseURL: "https://leaderboard-2225f.firebaseio.com",
+    projectId: "leaderboard-2225f",
+    storageBucket: "leaderboard-2225f.appspot.com",
+    messagingSenderId: "13229877992"
+  };
+  firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('Users');
+
+
+// Listen for form submit
+document.getElementById('SignUp').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var username = getInputVal('username');
+  var score = getInputVal('score');
+
+  // Save message
+  saveMessage(username, score);
+
+  // Clear form
+  document.getElementById('SignUp').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(username, score){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    username: username,
+    score:score,
+  });
+}
+
+
+
---
+++ main2.js
@@ -0,0 +1,40 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBf17vSw0ov3krg1a13x4ZPK2nnXAx43ww",
+    authDomain: "leaderboard-2225f.firebaseapp.com",
+    databaseURL: "https://leaderboard-2225f.firebaseio.com",
+    projectId: "leaderboard-2225f",
+    storageBucket: "leaderboard-2225f.appspot.com",
+    messagingSenderId: "13229877992"
+  };
+  firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('Users');
+
+messagesRef.on('value', getData);
+
+var arr = []
+
+function getData(data){
+  var scorelist = data.val();
+  var keys = Object.keys(scorelist);
+  for(var i = 0; i < keys.length; i++){
+    var k = keys[i];
+    var username = scorelist[k].username;
+    var time = scorelist[k].score;
+    var li = document.createElement('li', (username + ": " + time + "\n"));
+    jQuery(li).parent('scorelist');
+    console.log(username + ": " + time + "\n");
+    arr[i] = (username + ": " + time + "\n")
+    //document.write(username + ": " + time + "\n");
+  }
+}
+
+  var list = function(arr) {
+  for (var prop in arr) {
+    document.getElementById('scorelist').innerHTML += '<li>' + prop + '</li>';
+    console.log(prop);
+  }
+}
